e59a49f99d1aad263bbaf28a4e00c7b78ab73847
aldidaim21 <aldidiamfauzan15@gmail.com>
2023-09-17 21:53:18 +0700

    2

---
+++ python/aritmatika.py
@@ -0,0 +1,34 @@
+# operasi aritmatika
+c = input('masukan angka 1 :')
+d = input('masukan angka 2 :')
+
+a = int(c)
+b = int(d)
+
+# oprator tambah
+hasil = a+b
+print(hasil)
+
+# oprasi perkalian
+hasilpk = a*b
+print(hasilpk)
+
+# hasil pembagian
+hasilbg = a/b
+print(float(hasilbg))
+
+
+# oprator pangkat
+hasilpkt = a**b
+print(a**b)
+
+
+''' prioritas oprasi
+    1.()
+    2.exponen
+    3.perkalian
+    4.pembagian
+    5.pengurangan
+    6.modulus
+
+'''
---
+++ python/latihan_konversi_satuan_suhu.py
@@ -0,0 +1,22 @@
+# membuat latihan konversi saatuan suhu
+
+# program konversi celcius ke satuan lain
+print("\nPROGRAM KONVERSI SATUAN CELCIUS\n")
+
+# form input
+celcius = float(input('masukan suhu celcius:'))
+print('\nsuhu adalah', celcius, 'Celcius')
+
+# reamur
+reamur = (4/5) * celcius
+print('\nsuhu adalah', reamur, 'Reamur')
+
+# fahrenheit
+fh = ((9/5) * celcius)+32
+print('\nsuhu adalah', fh, 'Farenheit')
+
+# kelvin
+kelvin = celcius + 273
+print('\nsuhu adalah', kelvin, 'Farenheit')
+
+
