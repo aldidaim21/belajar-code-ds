13c5f8c7b3ddba2ca44663840dd98d31d5a1cdca
aldidaim21 <aldidiamfauzan15@gmail.com>
2023-09-22 21:49:26 +0700

    asep

---
+++ python/manipulasistringp1.py
@@ -0,0 +1,10 @@
+
+#menyambung string
+
+nama = 'ucup'
+nama2 = 'ucup'
+
+print(nama+ " " +nama2)
+
+#menghitung panjan string
+print(len(nama2))
--- python/pengenalanstring.py
+++ python/pengenalanstring.py
@@ -1,41 +1,68 @@
-#cara membuat string
+# #cara membuat string
 
 
-data = 'ini adalah string'
-print(data)
-print(type(data))
+# data = 'ini adalah string'
+# print(data)
+# print(type(data))
 
-#cara membuat string
-'''
-1.menggunakan singel qoute ''
-2.menggunaakan double quete
+# #cara membuat string
+# '''
+# 1.menggunakan singel qoute ''
+# 2.menggunaakan double quete
 
-'''
+# '''
 
-#print('asep')single quote
-#print("asep")double
+# #print('asep')single quote
+# #print("asep")double
 
-print("'halo apakabar'") #penggunakan dua duanya
-
-
-
-#membuat tanda ' sengaja
-print('mari shalat jum\'aat')
-
-
-#membuat blackshash
-print('c:\\user\\ucup')
+# print("'halo apakabar'") #penggunakan dua duanya
 
 
 
-#tab
-print('ucup\t\totong,jauhan')
+# #membuat tanda ' sengaja
+# print('mari shalat jum\'aat')
 
 
-#backspace
-print('ucup \botong','deketan')
+# #membuat blackshash
+# print('c:\\user\\ucup')
 
-#newline
-print('baris petama,\n baris kedua')
-print('baris pertama.\rbaris kedua')
-print('baris pertama.\r\nbaris kedua')
+
+
+# #tab
+# print('ucup\t\totong,jauhan')
+
+
+# #backspace
+# print('ucup \botong','deketan')
+
+# #newline
+# print('baris petama,\n baris kedua')
+# print('baris pertama.\rbaris kedua')
+# print('baris pertama.\r\nbaris kedua')
+
+
+# 3. string literak atau raw
+
+#hatihati
+print('C:\new folder')
+
+#menggunakan raw string
+print(r'C:\new folder')
+
+#literal string
+print("""
+
+nama:asep
+nama:udin
+
+
+""")
+
+
+#multiline literal string
+
+print(r"""
+
+asep\asep
+
+""")
